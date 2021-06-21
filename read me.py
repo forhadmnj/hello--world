bf749921f9a6b8d2d2518c9a58952a00ac6edbcb
forhadmnj <85519691+forhadmnj@users.noreply.github.com>
2021-06-21 16:57:06 +0600

    initial commit

---
+++ read me.py
@@ -0,0 +1,10 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Twinkle, twinkle, little star\n \t How I wonder what you are!\n \t\tUp above the world so high,\n \t\tLike a diamond in the sky.\nTwinlke, twinkle, little star,\n \t How I wonder what you are')
+Twinkle, twinkle, little star
+ 	 How I wonder what you are!
+ 		Up above the world so high,
+ 		Like a diamond in the sky.
+Twinlke, twinkle, little star,
+ 	 How I wonder what you are
+>>> 
